c02e150bc4dea9777e5ed66856b4f457e3b107f3
Yusuke Endoh <mame@ruby-lang.org>
2021-07-26 02:32:02 +0900

    Regenerate

--- docs/_next/static/rJGuodUQapGJOM9w1xD7q/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex-da6eb024a688d6c89673.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-f390dd4dc7b33a407478.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
