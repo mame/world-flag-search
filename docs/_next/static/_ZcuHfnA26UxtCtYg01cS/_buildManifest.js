0c918f0858f9c01e6388ef5a3a2ed77b9a752789
Yusuke Endoh <mame@ruby-lang.org>
2024-07-27 01:53:36 +0900

    Load the flags asynchronously
    Fixes #22

--- docs/_next/static/_ZcuHfnA26UxtCtYg01cS/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/472-785b9189c8ff4114.js","static/chunks/pages/index-3372d79574df61a7.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
